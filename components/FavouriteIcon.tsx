c2d3a27adafffcc3479421d80773c65a09f5f2d8
Ishwak Sharda <ishwak.sharda@gmail.com>
2021-08-02 15:00:55 +0530

    Distinct favourite icon component

---
+++ components/FavouriteIcon.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { TouchableWithoutFeedback } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useGlobal } from "../global/provider";
+
+const FavouriteIcon = ({ food_name, calories, onClick }) => {
+  const {
+    state: { favourites },
+  } = useGlobal();
+  const isActive = () =>
+    favourites.findIndex(
+      (a) => a.food_name === food_name && a.calories === calories
+    ) < 0
+      ? false
+      : true;
+  return (
+    <TouchableWithoutFeedback onPress={onClick}>
+      <AntDesign name={isActive() ? "star" : "staro"} size={20} />
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default FavouriteIcon;
